c972e4ecae72dda88e2770f11287388837ec38ac
mohammad sayadi <msdehnoo@gmail.com>
2023-04-09 14:50:53 +0430

    add base page footer,and sass base page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/scss/main.scss'
+import Navbar from './components/public/Navbar';
+import Footer from './components/public/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Footer/>
+  
     </div>
   );
 }
