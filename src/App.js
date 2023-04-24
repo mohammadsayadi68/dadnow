00d7e72f6e179e44434be103199fa80a444c45f8
mohammad sayadi <msdehnoo@gmail.com>
2023-04-24 16:13:46 +0430

    add fontello to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './assets/scss/main.scss'
 import Navbar from './components/public/Navbar';
 import Footer from './components/public/Footer';
+import "./fontello/css/fontello.css"
 
 function App() {
   return (
