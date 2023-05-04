4b26ded5db1d98b78734ee759c398b6ebc18ccc2
mohammad sayadi <msdehnoo@gmail.com>
2023-05-04 12:56:23 +0430

    add my tam to landing

--- src/components/pages/main/index.jsx
+++ src/components/pages/main/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NewNews from './new-news.jsx';
 import Dadpro from './dadpro.jsx';
+import Team from './team.jsx';
 
 const index = () => {
   return (
     <div>
         <NewNews/>
         <Dadpro/>
+        <Team/>
     </div>
   )
 }
---
+++ src/components/pages/main/team.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import circle from "../../../public/image/circle-yelow.svg";
+import triangleVector from "../../../public/image/triangle-vector.svg";
+import userImg from "../../../public/image/team-img.png";
+import Google from "../../../public/image/Google.svg";
+
+const team = () => {
+  return (
+    <div className="mx-100 team mt-5">
+      <div className="container-flouid">
+        <div className="triangleVector center">
+          <img src={triangleVector} className="" alt="" />
+        </div>
+    
+        <div className="row">
+          <div className="col-12 d-flex justify-content-between align-items-center">
+            <div className=" section-titre d-flex aligin-items-center">
+              <div className="border-right"></div>
+              <div className="mx-2"> تحریریه‌ی ما </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row d-flex justify-content-between">
+          <div className="col-md-3">
+            <div className="center flex-column">
+              <div className="center flex-column team-card">
+                <img src={userImg} className="team-card-img" alt="" />
+                <div className="team-card-titre">صدرا عظیم</div>
+                <div className="gray-title"> خبرنگار صفحات اجتماعی</div>
+                <div className="d-flex">
+                <div className="icon center">
+                    <img src={Google} className="" alt="" />
+                  </div>
+                  <div className="icon center">
+                    <i className="demo-icon icon-linkedin " />
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="center flex-column">
+              <div className="center flex-column team-card">
+                <img src={userImg} className="team-card-img" alt="" />
+                <div className="team-card-titre">صدرا عظیم</div>
+                <div className="gray-title"> خبرنگار صفحات اجتماعی</div>
+                <div className="d-flex">
+                <div className="icon center">
+                    <img src={Google} className="" alt="" />
+                  </div>
+                  <div className="icon center">
+                    <i className="demo-icon icon-linkedin " />
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="center flex-column">
+              <div className="center flex-column team-card">
+                <img src={userImg} className="team-card-img" alt="" />
+                <div className="team-card-titre">صدرا عظیم</div>
+                <div className="gray-title"> خبرنگار صفحات اجتماعی</div>
+                <div className="d-flex">
+                <div className="icon center">
+                    <img src={Google} className="" alt="" />
+                  </div>
+                  <div className="icon center">
+                    <i className="demo-icon icon-linkedin " />
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="center flex-column">
+              <div className="center flex-column team-card">
+                <img src={userImg} className="team-card-img" alt="" />
+                <div className="team-card-titre">صدرا عظیم</div>
+                <div className="gray-title"> خبرنگار صفحات اجتماعی</div>
+                <div className="d-flex">
+                <div className="icon center">
+                    <img src={Google} className="" alt="" />
+                  </div>
+                  <div className="icon center">
+                    <i className="demo-icon icon-linkedin " />
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="circle center">
+          <img class="" alt="" />
+          <img src={circle} class="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default team;
