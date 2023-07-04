e5bb91a5a1137bb8cb98898459ca160f8a5b6b2c
mohammad sayadi <msdehnoo@gmail.com>
2023-07-04 08:02:11 +0330

    edit navbar

--- src/components/public/Navbar.jsx
+++ src/components/public/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
 // import logo from "../../public/image/podilaw-mag.svg";
 import logo from "../../public/image/Podilaw-mag-dark.svg";
 import searchIcon from "../../public/image/search-normal.svg";
+import userIcon from "../../public/image/user.jpeg";
+import edit from "../../public/image/user-edit.svg";
+import logout from "../../public/image/logout.svg";
 
 const Navbar = () => {
+  document.body.addEventListener('click', console.log('dropdown') );
+  const user = null;
+  const [dropdown, setDropdown] = useState(false);
+  const toggleOpen = () => setDropdown(!dropdown);
+  
+  
   return (
     <div className="navbar">
       <div className="navbar-logo">
@@ -32,8 +42,12 @@ const Navbar = () => {
             </a>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-item-link" activeClassName='active' to="/multy-media">
-            اخبار چندرسانه ای
+            <NavLink
+              className="nav-item-link"
+              activeClassName="active"
+              to="/multy-media"
+            >
+              اخبار چندرسانه ای
             </NavLink>
           </li>
           <li className="nav-item">
@@ -47,9 +61,45 @@ const Navbar = () => {
         <div className="center navbar-left-search">
           <img src={searchIcon} alt="" className="navbar-left-search-icon" />
         </div>
-        <a href="" className="navbar-left-login-btn">
-          ورود/عضویت
-        </a>
+        {!user ? (
+          <a href="" className="navbar-left-login-btn">
+            ورود/عضویت
+          </a>
+        ) : (
+          <>
+            <div className="dropdown pointer">
+              <div className="d-flex align-items-center" onClick={toggleOpen}>
+                <i
+                  className={`demo-icon   gradient-text t-2 ${
+                    dropdown ? "icon-angle-up" : "icon-angle-down"
+                  }`}
+                />
+                 <span className="  gray-title f-12 mx-2">لیدا بابایی</span>
+                <img src={userIcon} alt="" className="nav-avatar" />
+              </div>
+              <div
+                className={`dropdown-menu ${dropdown ? "show" : ""}`}
+                aria-labelledby="dropdownMenuButton"
+              >
+                <NavLink className="dropdown-item" to='/'>
+                  <img src={edit} alt="" />
+                  
+                 <span className="mx-2"> مشاهده پروفایل</span>
+
+                </NavLink>
+                <NavLink className="nav-item-link" activeClassName="active" to="/">
+              {/* <i className="demo-icon icon-home" /> */}
+              صفحه اصلی
+            </NavLink>
+                <a className="dropdown-item mt-3" href="#">
+                <img src={logout} alt="" />
+
+                 <span className="mx-2"> خروج</span>
+                </a>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
